d64a88914fb9982efce19bf3156bc34362aea9a2
Flibble46uk <flibble46uk@yahoo.com>
2022-11-23 15:18:08 +0000

    For Raptor

---
+++ RaptorGaming/Actors.tsx
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="actors" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="actors.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="oscar" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="oscar_crouch" type="string" />
+    </properties>
+  </tile>
+  <tile id="2" />
+  <tile id="3">
+    <properties>
+      <property name="Name" value="statusbar_locked" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="level01_enemy_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="Name" value="level01_boss" type="string" />
+    </properties>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="Name" value="level01_bighead" type="string" />
+    </properties>
+  </tile>
+  <tile id="7" />
+  <tile id="8">
+    <properties>
+      <property name="Name" value="level01_enemy_02" type="string" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="Name" value="level01_turret_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="Name" value="level01_turret_02" type="string" />
+    </properties>
+  </tile>
+  <tile id="11">
+    <properties>
+      <property name="Name" value="level01_miniboss" type="string" />
+    </properties>
+  </tile>
+  <tile id="12">
+    <properties>
+      <property name="Name" value="level01_bullet_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="Name" value="level01_grenade_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="Name" value="oscar_laser_act" type="string" />
+    </properties>
+  </tile>
+  <tile id="15">
+    <properties>
+      <property name="Name" value="dummy_actor" type="string" />
+    </properties>
+  </tile>
+  <tile id="16" />
+  <tile id="17">
+    <properties>
+      <property name="Name" value="oscar_grenade_act" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="oscar_grenade_test" type="string" />
+    </properties>
+  </tile>
+  <tile id="19">
+    <properties>
+      <property name="Name" value="oscar_muzzle_act" type="string" />
+    </properties>
+  </tile>
+  <tile id="20">
+    <properties>
+      <property name="Name" value="oscar_gunhit_act" type="string" />
+    </properties>
+  </tile>
+  <tile id="21">
+    <properties>
+      <property name="Name" value="level01_enemy_03_spawner" type="string" />
+    </properties>
+  </tile>
+</tileset>
