d91f45e9e16b0e7b77feeb3769ba8dd73e68f6fb
Flibble46uk <flibble46uk@yahoo.com>
2022-12-08 23:40:50 +0000

    08/12/22

--- MultiplexTest-Mixel/Maps/test_map_01_extra.tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="test_map_01_extra.tileset" tilewidth="16" tileheight="16" tilecount="3" columns="3">
- <image source="test_map_01_extra.tileset.png" trans="000000" width="48" height="16"/>
-</tileset>
