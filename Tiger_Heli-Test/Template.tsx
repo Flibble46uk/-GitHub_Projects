3ac13d37c2eb6a71b8b22a5f91cc1abbf18e7493
Flibble46uk <flibble46uk@yahoo.com>
2022-12-23 19:42:22 +0000

    29/12/22

---
+++ Tiger_Heli-Test/Template.tsx
@@ -0,0 +1,165 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="templates" tilewidth="256" tileheight="256" tilecount="256" columns="0">
+  <image source="templates.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="00_dummyplayer" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="00_dummycamera" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="00_dummyactor" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="hero_loaded" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="01_crater_small" type="string" />
+    </properties>
+  </tile>
+  <tile id="5" />
+  <tile id="6">
+    <properties>
+      <property name="Name" value="01_dec_road_barrier" type="string" />
+    </properties>
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="Name" value="01_militarycar_small_left" type="string" />
+    </properties>
+  </tile>
+  <tile id="8">
+    <properties>
+      <property name="Name" value="heli_truck" type="string" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="Name" value="01_militarycar_small_right" type="string" />
+    </properties>
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="Name" value="01_militarycar_small_up" type="string" />
+    </properties>
+  </tile>
+  <tile id="11">
+    <properties>
+      <property name="Name" value="01_civcar_silver_up" type="string" />
+    </properties>
+  </tile>
+  <tile id="12">
+    <properties>
+      <property name="Name" value="01_civcar_red_down" type="string" />
+    </properties>
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="Name" value="01_civcar_blue_right" type="string" />
+    </properties>
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="Name" value="01_civcar_red_left" type="string" />
+    </properties>
+  </tile>
+  <tile id="15">
+    <properties>
+      <property name="Name" value="01_dec_rail_barrier_top" type="string" />
+    </properties>
+  </tile>
+  <tile id="16">
+    <properties>
+      <property name="Name" value="01_dec_rail_barrier_bottom" type="string" />
+    </properties>
+  </tile>
+  <tile id="17">
+    <properties>
+      <property name="Name" value="01_dec_green_tent" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="01_dec_brown_tent" type="string" />
+    </properties>
+  </tile>
+  <tile id="19">
+    <properties>
+      <property name="Name" value="01_militaryair_fighter01_left" type="string" />
+    </properties>
+  </tile>
+  <tile id="20">
+    <properties>
+      <property name="Name" value="01_militaryair_fighter01_up" type="string" />
+    </properties>
+  </tile>
+  <tile id="21">
+    <properties>
+      <property name="Name" value="01_dec_generator_up" type="string" />
+    </properties>
+  </tile>
+  <tile id="22">
+    <properties>
+      <property name="Name" value="01_dec_generator_left" type="string" />
+    </properties>
+  </tile>
+  <tile id="23">
+    <properties>
+      <property name="Name" value="01_militaryair_fighter01_left_moving" type="string" />
+    </properties>
+  </tile>
+  <tile id="24">
+    <properties>
+      <property name="Name" value="turret_static" type="string" />
+    </properties>
+  </tile>
+  <tile id="25">
+    <properties>
+      <property name="Name" value="exp_dummy" type="string" />
+    </properties>
+  </tile>
+  <tile id="26">
+    <properties>
+      <property name="Name" value="exp_ground_crater" type="string" />
+    </properties>
+  </tile>
+  <tile id="27">
+    <properties>
+      <property name="Name" value="exp_ground" type="string" />
+    </properties>
+  </tile>
+  <tile id="28">
+    <properties>
+      <property name="Name" value="hero_takeoff" type="string" />
+    </properties>
+  </tile>
+  <tile id="29">
+    <properties>
+      <property name="Name" value="hero_takeoff01" type="string" />
+    </properties>
+  </tile>
+  <tile id="30">
+    <properties>
+      <property name="Name" value="hero_takeoff02" type="string" />
+    </properties>
+  </tile>
+  <tile id="31">
+    <properties>
+      <property name="Name" value="01_dec_red_roof_tube" type="string" />
+    </properties>
+  </tile>
+  <tile id="32">
+    <properties>
+      <property name="Name" value="01_dec_red_tent_small" type="string" />
+    </properties>
+  </tile>
+</tileset>
