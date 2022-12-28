6e8993b28e625cfd4fdc08924cb9622f9aa07832
Flibble46uk <flibble46uk@yahoo.com>
2022-12-28 19:52:40 +0000

    BasementApe 26/12/22

---
+++ Rastan-Test/Template.tsx
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="templates" tilewidth="256" tileheight="256" tilecount="256" columns="0">
+  <image source="templates.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="dummycamera" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="dummyplayer" type="string" />
+    </properties>
+  </tile>
+  <tile id="2" />
+  <tile id="3" />
+  <tile id="4">
+    <properties>
+      <property name="Name" value="hero_legs_walk" type="string" />
+    </properties>
+  </tile>
+  <tile id="5" />
+  <tile id="6" />
+  <tile id="7" />
+  <tile id="8" />
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12" />
+  <tile id="13" />
+  <tile id="14" />
+  <tile id="15" />
+  <tile id="16">
+    <properties>
+      <property name="Name" value="actor_stoneblock" type="string" />
+    </properties>
+  </tile>
+  <tile id="17" />
+  <tile id="18" />
+  <tile id="19" />
+  <tile id="20" />
+  <tile id="21" />
+  <tile id="22" />
+  <tile id="23" />
+  <tile id="24" />
+  <tile id="25" />
+  <tile id="26" />
+  <tile id="27" />
+  <tile id="28" />
+  <tile id="29" />
+  <tile id="30" />
+  <tile id="31" />
+  <tile id="32">
+    <properties>
+      <property name="Name" value="wizard_001" type="string" />
+    </properties>
+  </tile>
+  <tile id="33">
+    <properties>
+      <property name="Name" value="hero_torso" type="string" />
+    </properties>
+  </tile>
+  <tile id="34">
+    <properties>
+      <property name="Name" value="hero_torso_downsword" type="string" />
+    </properties>
+  </tile>
+  <tile id="35">
+    <properties>
+      <property name="Name" value="hero_legs_crouch" type="string" />
+    </properties>
+  </tile>
+</tileset>
