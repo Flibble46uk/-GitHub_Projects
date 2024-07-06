f4458b2df180e7692e94ea75ac7bfa1c3850ab56
Flibble46uk <flibble46uk@yahoo.com>
2024-07-06 09:18:44 +0100

    06/07/26
    Anims

---
+++ StarFighter/Maps/Intro/Intro.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Intro" tilewidth="16" tileheight="16" tilecount="176" columns="16">
+ <image source="Intro.png" trans="000000" width="256" height="176"/>
+</tileset>
