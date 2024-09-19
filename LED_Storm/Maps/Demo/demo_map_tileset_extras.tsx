3426ac6e51310d64646bf55ed1b89a70c59d0b7d
Flibble46uk <flibble46uk@yahoo.com>
2024-09-19 08:09:31 +0100

    LED_Storm For Earok
    19/09/24

---
+++ LED_Storm/Maps/Demo/demo_map_tileset_extras.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="demo_map_tileset_extras" tilewidth="16" tileheight="16" tilecount="720" columns="15">
+ <image source="demo_map_tileset_extras.png" trans="000000" width="240" height="768"/>
+</tileset>
---
+++ LED_Storm/Maps/Demo/demo_map_tileset_extras_02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="demo_map_tileset_extras_02" tilewidth="16" tileheight="16" tilecount="390" columns="10">
+ <image source="demo_map_tileset_extras_02.png" trans="000000" width="160" height="624"/>
+</tileset>
