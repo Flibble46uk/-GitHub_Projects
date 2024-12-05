bf35b278df4459e3e4a51a751cc92f2e5158b57a
Flibble46uk <flibble46uk@yahoo.com>
2024-12-05 16:15:36 +0000

    Frogger
    First test

---
+++ Frogger-Dev/Template.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="templates" tilewidth="256" tileheight="256" tilecount="256" columns="0">
+  <image source="templates.png" width="256" height="256" />
+  <tile id="0" />
+  <tile id="1">
+    <properties>
+      <property name="Name" value="act_car_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="act_car_02" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="act_car_03" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="act_car_04" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="Name" value="act_car_05" type="string" />
+    </properties>
+  </tile>
+  <tile id="6" />
+  <tile id="7" />
+  <tile id="8" />
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12" />
+  <tile id="13" />
+  <tile id="14" />
+  <tile id="15" />
+  <tile id="16" />
+  <tile id="17">
+    <properties>
+      <property name="Name" value="act_turt_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="act_log_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="19">
+    <properties>
+      <property name="Name" value="act_log_02" type="string" />
+    </properties>
+  </tile>
+  <tile id="20">
+    <properties>
+      <property name="Name" value="act_log_03" type="string" />
+    </properties>
+  </tile>
+</tileset>
