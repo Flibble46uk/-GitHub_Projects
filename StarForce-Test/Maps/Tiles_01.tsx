78a9f5111cd95cfe5f9ee17f972dce289e69015b
Flibble46uk <flibble46uk@yahoo.com>
2025-01-21 20:17:11 +0000

    Does this glitch?

---
+++ StarForce-Test/Maps/Tiles_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Tiles_01" tilewidth="16" tileheight="16" tilecount="176" columns="16">
+ <image source="Tiles_01.png" trans="000000" width="256" height="176"/>
+</tileset>
