719872be54ddd91eef496943e4272dbb85390dce
Flibble46uk <flibble46uk@yahoo.com>
2025-01-22 02:34:15 +0000

    This any better?
    Made no noticable changes.

---
+++ StarForce-Test/Maps/Tiles_all.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Tiles_all" tilewidth="16" tileheight="16" tilecount="1280" columns="20">
+ <image source="Tiles_all.png" trans="000000" width="320" height="1024"/>
+</tileset>
