22fa7e7f0c7fb9f3713243b6acf5509c14f6cbf2
Flibble46uk <flibble46uk@yahoo.com>
2025-02-01 09:41:11 +0000

    Starforce-CD32 audio test
    Can you get this to compile to a CD and run?

---
+++ StarForce_Test/Maps/AMIGA_GFX_32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="AMIGA_GFX_32" tilewidth="16" tileheight="16" tilecount="3840" columns="40">
+ <image source="AMIGA_GFX_32.png" width="640" height="1536"/>
+</tileset>
