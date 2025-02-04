fade37b7a9e86233c73f898abf1f98c9b69e7b05
Flibble46uk <flibble46uk@yahoo.com>
2025-02-04 07:39:32 +0000

    Star Force 04/02/25 07.40

--- StarForce_Test/Template.tsx
+++ StarForce_Test/Template.tsx
@@ -11,4 +11,39 @@
       <property name="Name" value="en_01" type="string" />
     </properties>
   </tile>
+  <tile id="2" />
+  <tile id="3" />
+  <tile id="4" />
+  <tile id="5" />
+  <tile id="6" />
+  <tile id="7" />
+  <tile id="8" />
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12" />
+  <tile id="13" />
+  <tile id="14" />
+  <tile id="15" />
+  <tile id="16" />
+  <tile id="17" />
+  <tile id="18" />
+  <tile id="19" />
+  <tile id="20" />
+  <tile id="21" />
+  <tile id="22" />
+  <tile id="23" />
+  <tile id="24" />
+  <tile id="25" />
+  <tile id="26" />
+  <tile id="27" />
+  <tile id="28" />
+  <tile id="29" />
+  <tile id="30" />
+  <tile id="31" />
+  <tile id="32">
+    <properties>
+      <property name="Name" value="build_test" type="string" />
+    </properties>
+  </tile>
 </tileset>
