2ca1990072c2ec628e3a50812d15970181980f88
Flibble46uk <flibble46uk@yahoo.com>
2025-02-15 13:28:53 +0000

    Star Force 15/02/25 13.30

--- StarForce_Test/Blocks.tsx
+++ StarForce_Test/Blocks.tsx
@@ -21,4 +21,84 @@
       <property name="Name" value="blank" type="string" />
     </properties>
   </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="!b01shadow" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="Name" value="b01" type="string" />
+    </properties>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="Name" value="b02" type="string" />
+    </properties>
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="Name" value="b03" type="string" />
+    </properties>
+  </tile>
+  <tile id="8">
+    <properties>
+      <property name="Name" value="b04" type="string" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="Name" value="b05" type="string" />
+    </properties>
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="Name" value="b06" type="string" />
+    </properties>
+  </tile>
+  <tile id="11">
+    <properties>
+      <property name="Name" value="b07" type="string" />
+    </properties>
+  </tile>
+  <tile id="12">
+    <properties>
+      <property name="Name" value="b08" type="string" />
+    </properties>
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="Name" value="b09" type="string" />
+    </properties>
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="Name" value="b10" type="string" />
+    </properties>
+  </tile>
+  <tile id="15">
+    <properties>
+      <property name="Name" value="b11" type="string" />
+    </properties>
+  </tile>
+  <tile id="16">
+    <properties>
+      <property name="Name" value="b12" type="string" />
+    </properties>
+  </tile>
+  <tile id="17">
+    <properties>
+      <property name="Name" value="b13" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="b14" type="string" />
+    </properties>
+  </tile>
+  <tile id="19">
+    <properties>
+      <property name="Name" value="b15" type="string" />
+    </properties>
+  </tile>
 </tileset>
