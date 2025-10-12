a0fe44d803ce39c821c4244dd2424e3f35000ba6
Flibble46uk <flibble46uk@yahoo.com>
2025-10-12 11:07:42 +0100

    Enduro_Racer
    First check in.

---
+++ Enduro_Racer/Blocks.tsx
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blocks" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="blocks.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="digit_0" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="digit_1" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="digit_2" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="digit_3" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="digit_4" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="Name" value="digit_5" type="string" />
+    </properties>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="Name" value="digit_6" type="string" />
+    </properties>
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="Name" value="digit_7" type="string" />
+    </properties>
+  </tile>
+  <tile id="8">
+    <properties>
+      <property name="Name" value="digit_8" type="string" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="Name" value="digit_9" type="string" />
+    </properties>
+  </tile>
+</tileset>
