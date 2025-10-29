4a23735101926347b58ef117f9fc8c1d49806a77
Flibble46uk <flibble46uk@yahoo.com>
2025-10-29 21:27:51 +0000

    29/10/25
    Brackets for Earok

--- Fantasy_Zone/Blocks.tsx
+++ Fantasy_Zone/Blocks.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="utf-8"?>
 <tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blocks" tilewidth="16" tileheight="16" tilecount="256" columns="0">
   <image source="blocks.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="solid" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="lvl_01_carrier_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="lvl_01_carrier_02" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="lvl_01_carrier_03" type="string" />
+    </properties>
+  </tile>
 </tileset>
