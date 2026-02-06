f030d166fb5929f0cd584780e88a8a97078bca02
Flibble46uk <flibble46uk@yahoo.com>
2026-02-06 00:44:28 +0000

    BOB_Score
    Thought i had it figured, NOPE.

---
+++ Bob_Score/Maps/Blank.tileset.tsx
@@ -0,0 +1,356 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blank" tilewidth="16" tileheight="16" tilecount="16" columns="16">
+  <image source="blank.tileset.png" width="256" height="16" />
+  <tile id="0">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="8">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="11">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="12">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+  <tile id="15">
+    <properties>
+      <property name="IsSolid" value="False" type="bool" />
+      <property name="IsPlatform" value="False" type="bool" />
+      <property name="MD_Palette_TL" type="string" />
+      <property name="MD_Palette_TR" type="string" />
+      <property name="MD_Palette_BL" type="string" />
+      <property name="MD_Palette_BR" type="string" />
+      <property name="MD_FG_Priority_TL" value="True" type="bool" />
+      <property name="MD_FG_Priority_TR" value="True" type="bool" />
+      <property name="MD_FG_Priority_BL" value="True" type="bool" />
+      <property name="MD_FG_Priority_BR" value="True" type="bool" />
+      <property name="MD_BG_Priority_TL" value="False" type="bool" />
+      <property name="MD_BG_Priority_TR" value="False" type="bool" />
+      <property name="MD_BG_Priority_BL" value="False" type="bool" />
+      <property name="MD_BG_Priority_BR" value="False" type="bool" />
+      <property name="Neo_Palette" type="string" />
+      <property name="Neo_AutoAnim" value="False" type="bool" />
+      <property name="AnimateProperties" value="False" type="bool" />
+    </properties>
+    <animation />
+  </tile>
+</tileset>
